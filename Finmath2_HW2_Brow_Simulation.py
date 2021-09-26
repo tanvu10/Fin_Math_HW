ad3e8ee57f5a6d61db2e499643664b95dcefd5af
tanvu10 <86453999+tanvu10@users.noreply.github.com>
2021-09-26 15:19:50 +0700

    Initial commit

---
+++ Finmath2_HW2_Brow_Simulation.py
@@ -0,0 +1,33 @@
+import numpy as np
+from numpy import random
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+
+
+#simulation for 1 k_step playing
+def simulation(step):
+    random_list = []
+    for i in range(step+1):
+        if i == 0:
+            random_list.append(0)
+        else:
+            random_list.append(0.3 + random.normal(loc = 0, scale = 1, size =1 ) + random_list[i-1])
+    return random_list
+
+v1 = simulation(10)
+plt.plot(v1)
+plt.show()
+
+
+#n time playing simulation to calculate probability
+def proba_k_step(step, time_simu):
+    proba_list = []
+    for i in range(time_simu):
+        proba_list.append(simulation(step)[-1])
+    proba = len([j for j in proba_list if j >0])/len(proba_list)
+    print(proba)
+
+#probability from simulation
+proba_k_step(10, 100000)
+#probability from formula:
+print(1- norm.cdf(-3/np.sqrt(10)))
